44c97b995a96fe3fb1bb26adb8a81a7c2c79fa38
JesperDannath <jdannath@techfak.uni-bielefeld.de>
2023-09-06 16:33:28 +0200

    Small changes to install-script

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -19,14 +19,22 @@ fi
 
 
 #Frontend dependencies (Javascript/Angular)
-sudo apt-get install node
-sudo apt install npm
-sudo npm cache clean -fs
-sudo npm install -g n
-sudo n stable
-sudo npm install -g @angular/cli
-cd frontend
-npm install
+#sudo apt-get install node
+#sudo apt install npm
+#sudo npm cache clean -fs
+#sudo npm install -g n
+#sudo n stable
+#sudo npm install -g @angular/cli
+sudo apt-get install -y ca-certificates curl gnupg
+sudo mkdir -p /etc/apt/keyrings
+curl -fsSL https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key | sudo gpg --dearmor -o /etc/apt/keyrings/nodesource.gpg
+NODE_MAJOR=20
+echo "deb [signed-by=/etc/apt/keyrings/nodesource.gpg] https://deb.nodesource.com/node_$NODE_MAJOR.x nodistro main" | sudo tee /etc/apt/sources.list.d/nodesource.list
+sudo apt-get update
+sudo apt-get install nodejs -y
+
+cd frontend/its_ui
+sudo npm install
 
 #Database dependencies (MongoDB)
 wget https://repo.mongodb.org/apt/ubuntu/dists/jammy/mongodb-org/6.0/multiverse/binary-amd64/mongodb-org-server_6.0.8_amd64.deb
