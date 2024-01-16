d726fc6f7b2100f14c6ccacf52e86e8776d14652
JesperDannath <jdannath@techfak.uni-bielefeld.de>
2024-01-16 18:39:29 +0100

    Ollama integration for 7b model

--- api/models/pedagogical/prototype.py
+++ api/models/pedagogical/prototype.py
@@ -2,15 +2,57 @@ from models.pedagogical.base import Base_pedagogical_model
 from models.pedagogical.content_selection.prototype import Prototype_task_selector
 from users.schemas import User
 from submissions.schemas import Code_submission
+from db import database
+import aiohttp
+import json
 
 class Prototype_pedagogical_model(Base_pedagogical_model):
 
     def __init__(self):
         self.task_selector = Prototype_task_selector
         self.feedback_method = "LLM-next-step"
+        self.task_summaries = {}
 
     async def select_task(self, user: User):
         return await self.task_selector.select(user)
     
-    async def give_feedback(self, submission: Code_submission):
-        return("Feedback")
+    async def give_feedback(self, submission: Code_submission, ollama_url="http://localhost:11434"):
+        """This implementation of give_feedback uses the ollama API to receive llm-generated next-step feedback.
+
+        Args:
+            submission (Code_submission): The users code submission
+            api_url (str, optional): Ollama API url to be used for requests. Defaults to "http://localhost:11434".
+        """
+        #Retrieve task info and create prompt.
+        task_json = await database.get_task(submission.task_unique_name)
+        previous_step = task_json.prefix+submission.code
+        task = task_json.task
+        instruction = self.create_instruction(previous_step, task=task)
+        #Make request and receive/process LLM-answer
+        async with aiohttp.ClientSession() as session:
+    #     if submission.task_unique_name not in self.task_summaries.keys():
+    #         payload = {
+    #             "model": "codellama-7b-nxt",
+    #             "prompt": "Please summarize the following programming task one sentence, focussing only on the actual assignment specs:\nTask:\n'{task}'"
+    #         }
+    #         async with session.post(f"{ollama_url}/api/generate", json=payload) as response:
+    #             summary = await response.text()
+    #             summary = json.loads(feedback)
+    #             self.task_summaries[submission.task_unique_name] = summary
+            instruction = self.create_instruction(previous_step, task="Task should be here") #TODO: get short version of tasks or differentiate between local and server.
+            payload = {
+                    "model": "codellama-7b-nxt",
+                    "prompt": instruction,
+                    "stream": False,
+                    "options": {"num_predict": 100,
+                                "stop": ["<s>", "</s>", "[INST]", "[/INST]", "<<SYS>>", "<</SYS>>"]}
+                }
+            async with session.post(f"{ollama_url}/api/generate", json=payload) as response:
+                feedback = await response.text()
+                feedback = json.loads(feedback)
+        return(feedback["response"])
+    
+    def create_instruction(self, previous_step, task_ins="Consider the following programming task:", task="",
+                                 inst="Predict a reasonable next step of the student program. It is important to only predict the next step and not the complete solution! Also there should be only the edited code and no further explanations. The current program state is:", 
+                                b_inst="[INST]", e_inst="[/INST]"):
+        return f"""<s> {b_inst} {task_ins}\n"{task}"\n{inst}\n{previous_step} {e_inst}\nNext Step:\n"""
