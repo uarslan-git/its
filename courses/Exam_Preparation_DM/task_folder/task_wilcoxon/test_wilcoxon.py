e9f9331454e4ccbd000fc21843a2e60d9bb282e2
JesperDannath <jdannath@techfak.uni-bielefeld.de>
2024-01-26 11:58:18 +0100

    Fix wilcoxon test

--- courses/Exam_Preparation_DM/task_folder/task_wilcoxon/test_wilcoxon.py
+++ courses/Exam_Preparation_DM/task_folder/task_wilcoxon/test_wilcoxon.py
@@ -1,14 +1,14 @@
 from example_solution import wilcoxon_signed_rank_test
-import numpy as np
-from scipy.stats import wilcoxon
 #!cut_imports!#
 def test_wilcoxon():
+    import numpy as np
+    from scipy.stats import wilcoxon
     x = [1,2,3,4,5, 6]
     y = [-2,-1,0, 1, 2, 3]
     res = wilcoxon_signed_rank_test(x, y, alpha=0.05)
 
     #Test Output type
-    #assert type(res) in [bool, np.bool_], f"Return type should be boolean, not {type(res)}"
+    assert type(res) in [bool, np.bool_], f"Return type should be boolean, not {type(res)}"
 
     #Test invalid alpha values
     valid_aplha = False
