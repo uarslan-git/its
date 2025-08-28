7b7ae073addf9736a98ba126e99485d4248ab611
Umut Arslan <uarslan@techfak.uni-bielefeld.de>
2025-08-28 12:39:19 +0200

    Split introduction_to_ml course

---
+++ courses/split_course/Intro_to_ML_1/task_folder/task_graph_test/example_solution.py
@@ -0,0 +1,6 @@
+#!plot_function!#
+import matplotlib.pyplot as plt
+def plot_points():
+#!prefix!#
+    plt.plot([1, 3, 2, 4])
+    plt.show()
---
+++ courses/split_course/Intro_to_ML_1/task_folder/task_graph_test/test_graph_test.py
@@ -0,0 +1,7 @@
+import example_solution
+from .models.plt_wrappers.plt_wrapper import PLT_Wrapper
+#!cut_imports!#
+def test_graph_test():
+    # TODO
+    X = [1, 3, 2, 4]
+    assert True
