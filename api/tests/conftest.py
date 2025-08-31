740fb30e20b9728bdaed4a83a5ceb89d8e6243a6
Umut Arslan <uarslan@techfak.uni-bielefeld.de>
2025-08-31 11:05:53 +0200

    init repo

--- api/tests/conftest.py
+++ api/tests/conftest.py
@@ -1,8 +1,12 @@
 import asyncio
-import pytest
+import pytest_asyncio
+from httpx import AsyncClient
+from main import app
+from db import database
+from users.schemas import UserCreate
+import datetime
 
-
-@pytest.fixture(scope="session")
+@pytest_asyncio.fixture(scope="session")
 def event_loop():
     try:
         loop = asyncio.get_running_loop()
@@ -10,3 +14,27 @@ def event_loop():
         loop = asyncio.new_event_loop()
     yield loop
     loop.close()
+
+@pytest_asyncio.fixture(scope="session")
+async def authenticated_client():
+    async with AsyncClient(app=app, base_url="http://test") as client:
+        # Create a test user
+        user_in = UserCreate(
+            email="testuser@example.com",
+            password="password",
+            username="testuser",
+            verification_email="testuser@example.com",
+            current_course="Intro_to_Py",
+            enrolled_courses=["Intro_to_Py"],
+            register_datetime={"$date": "2024-01-01T00:00:00.000Z"},
+            settings={}
+        )
+        await database.db["users"].insert_one(user_in.dict())
+
+        # Log in to get the cookie
+        response = await client.post("/api/auth/jwt/login", data={"username": "testuser@example.com", "password": "password"})
+        cookie = response.cookies.get("fastapi-users-auth")
+
+        # Create a new client with the cookie
+        client.cookies.set("fastapi-users-auth", cookie)
+        yield client
--- api/tests/test_handle_submissions.py
+++ api/tests/test_handle_submissions.py
@@ -1,44 +1,29 @@
-from fastapi.testclient import TestClient
 import json
-from submissions.handle_submissions import router
+from unittest.mock import AsyncMock, patch
+
 import pytest
 from httpx import AsyncClient
 
-#TODO: Implement User-logic for this test: create a test user and store cookie - send submit request with cookie.
-from fastapi.testclient import TestClient
-import json
 from main import app
-import pytest
-from httpx import AsyncClient
 
-#TODO: Implement User-logic for this test: create a test user and store cookie - send submit request with cookie.
-async def submit_code(code, expected_status=1, expected_message_prefix="", task_id=1):
-    async with AsyncClient(app=app, base_url="http://test") as ac:
-        response = await ac.post("/submit", content=json.dumps({"task_unique_name": task_id, 
-                                                                     "code": code, 
-                                                                     "log": "True", 
+
+@pytest.mark.asyncio
+async def test_incorrect_code_submit(authenticated_client: AsyncClient):
+    response = await authenticated_client.post("/api/submit", content=json.dumps({"task_unique_name": 1,
+                                                                     "code": "def factorial(n): return 1",
+                                                                     "log": "True",
                                                                      "submission_id": "test",
                                                                      "submission_time": ""}),
                                                                      )
     payload = response.json()
-    print(payload)
-    assert payload['test_results'][0]['status'] == expected_status, "Incorrect status code"
+    assert payload['test_results'][0]['status'] == 0, "Incorrect status code"
     message = payload['test_results'][0]['message']
-    assert message.startswith(expected_message_prefix), f"Incorrect message prefix: {message} instead of {expected_message_prefix}"
+    assert message.startswith("Test failure"), f"Incorrect message prefix: {message} instead of Test failure"
 
 
 @pytest.mark.asyncio
-async def test_incorrect_code_submit():
-    code = """
-def factorial(n):
-    return(1)
-"""
-    await submit_code(code, 0, "Test failure")
-
-
-@pytest.mark.asyncio
-async def test_correct_code_submit():
-    code = """
+async def test_correct_code_submit(authenticated_client: AsyncClient):
+    code = '''
 def factorial(n):
     if n < 0:
         raise ValueError("Factorial is undefined for negative numbers.")
@@ -49,69 +34,126 @@ def factorial(n):
         for i in range(1, n + 1):
             result *= i
         return result
-"""
-    await submit_code(code, 1, "Test success")
+'''
+    response = await authenticated_client.post("/api/submit", content=json.dumps({"task_unique_name": 1,
+                                                                     "code": code,
+                                                                     "log": "True",
+                                                                     "submission_id": "test",
+                                                                     "submission_time": ""}),
+                                                                     )
+    payload = response.json()
+    assert payload['test_results'][0]['status'] == 1, "Incorrect status code"
+    message = payload['test_results'][0]['message']
+    assert message.startswith("Test success"), f"Incorrect message prefix: {message} instead of Test success"
 
-# Benjamin fragen!
+
 @pytest.mark.asyncio
-async def test_syntax_error_handling():
-    code="""
-def factorial(n:
+async def test_syntax_error_handling(authenticated_client: AsyncClient):
+    code = '''
+def factorial(n):
     if n == 0:
         return 1
     else:
         return n * factorial(n-1)
-"""
-    await submit_code(code, 0, "Error or Exception")
+'''
+    response = await authenticated_client.post("/api/submit", content=json.dumps({"task_unique_name": 1,
+                                                                     "code": code,
+                                                                     "log": "True",
+                                                                     "submission_id": "test",
+                                                                     "submission_time": ""}),
+                                                                     )
+    payload = response.json()
+    assert payload['test_results'][0]['status'] == 0, "Incorrect status code"
+    message = payload['test_results'][0]['message']
+    assert message.startswith("Error or Exception"), f"Incorrect message prefix: {message} instead of Error or Exception"
+
 
 @pytest.mark.asyncio
-async def test_import_statement_refusal():
-    code = """
+async def test_import_statement_refusal(authenticated_client: AsyncClient):
+    code = '''
 import os
 def factorial(n):
     if n == 0:
         return 1
     else:
         return n * factorial(n-1)
-"""
-    await submit_code(code, 0, "Error or Exception: Imports are not allowed in this context")
+'''
+    response = await authenticated_client.post("/api/submit", content=json.dumps({"task_unique_name": 1,
+                                                                     "code": code,
+                                                                     "log": "True",
+                                                                     "submission_id": "test",
+                                                                     "submission_time": ""}),
+                                                                     )
+    payload = response.json()
+    assert payload['test_results'][0]['status'] == 0, "Incorrect status code"
+    message = payload['test_results'][0]['message']
+    assert message.startswith("Error or Exception: Imports are not allowed in this context"), f"Incorrect message prefix: {message}"
 
 
 @pytest.mark.asyncio
-async def test_from_import_statement_refusal():
-    code="""
+async def test_from_import_statement_refusal(authenticated_client: AsyncClient):
+    code = '''
 from os import system
 def factorial(n):
     if n == 0:
         return 1
     else:
         return n * factorial(n-1)
-"""
-    await submit_code(code, 0, "Error or Exception: Imports are not allowed in this context")
+'''
+    response = await authenticated_client.post("/api/submit", content=json.dumps({"task_unique_name": 1,
+                                                                     "code": code,
+                                                                     "log": "True",
+                                                                     "submission_id": "test",
+                                                                     "submission_time": ""}),
+                                                                     )
+    payload = response.json()
+    assert payload['test_results'][0]['status'] == 0, "Incorrect status code"
+    message = payload['test_results'][0]['message']
+    assert message.startswith("Error or Exception: Imports are not allowed in this context"), f"Incorrect message prefix: {message}"
 
 
 @pytest.mark.asyncio
-async def test_delete_refusal():
-    code="""
+async def test_delete_refusal(authenticated_client: AsyncClient):
+    code = '''
 def factorial(n):
     return(n)
 del test_result
-"""
-    await submit_code(code, 0, "Error or Exception: Deletes are not allowed in this context")
+'''
+    response = await authenticated_client.post("/api/submit", content=json.dumps({"task_unique_name": 1,
+                                                                     "code": code,
+                                                                     "log": "True",
+                                                                     "submission_id": "test",
+                                                                     "submission_time": ""}),
+                                                                     )
+    payload = response.json()
+    assert payload['test_results'][0]['status'] == 0, "Incorrect status code"
+    message = payload['test_results'][0]['message']
+    assert message.startswith("Error or Exception: Deletes are not allowed in this context"), f"Incorrect message prefix: {message}"
+
 
 @pytest.mark.asyncio
-async def test_global_refusal():
-    code="""
+async def test_global_refusal(authenticated_client: AsyncClient):
+    code = '''
 def factorial(n):
     return(n)
 global test_result
 test_result = 1
-"""
-    await submit_code(code, 0, "Error or Exception: Global Scope is not allowed")
+'''
+    response = await authenticated_client.post("/api/submit", content=json.dumps({"task_unique_name": 1,
+                                                                     "code": code,
+                                                                     "log": "True",
+                                                                     "submission_id": "test",
+                                                                     "submission_time": ""}),
+                                                                     )
+    payload = response.json()
+    assert payload['test_results'][0]['status'] == 0, "Incorrect status code"
+    message = payload['test_results'][0]['message']
+    assert message.startswith("Error or Exception: Global Scope is not allowed"), f"Incorrect message prefix: {message}"
+
 
 @pytest.mark.asyncio
-async def test_nonlocal_refusal():
-    code="""
+async def test_nonlocal_refusal(authenticated_client: AsyncClient):
+    code = '''
 def factorial(n):
     test_result = 0
     def change_result():
@@ -119,140 +161,95 @@ def factorial(n):
         test_result = 1
     change_result()
     return(n)
-"""
-    await submit_code(code, 0, "Error or Exception: Nonlocal Scope is not allowed")
+'''
+    response = await authenticated_client.post("/api/submit", content=json.dumps({"task_unique_name": 1,
+                                                                     "code": code,
+                                                                     "log": "True",
+                                                                     "submission_id": "test",
+                                                                     "submission_time": ""}),
+                                                                     )
+    payload = response.json()
+    assert payload['test_results'][0]['status'] == 0, "Incorrect status code"
+    message = payload['test_results'][0]['message']
+    assert message.startswith("Error or Exception: Nonlocal Scope is not allowed"), f"Incorrect message prefix: {message}"
+
 
 @pytest.mark.asyncio
-async def test_exec_refusal():
-    code="""
+async def test_exec_refusal(authenticated_client: AsyncClient):
+    code = '''
 def factorial(n):
     return(n)
 exec("import os")
-"""
-    await submit_code(code, 0, "Error or Exception: exec() is not allowed in this context")
+'''
+    response = await authenticated_client.post("/api/submit", content=json.dumps({"task_unique_name": 1,
+                                                                     "code": code,
+                                                                     "log": "True",
+                                                                     "submission_id": "test",
+                                                                     "submission_time": ""}),
+                                                                     )
+    payload = response.json()
+    assert payload['test_results'][0]['status'] == 0, "Incorrect status code"
+    message = payload['test_results'][0]['message']
+    assert message.startswith("Error or Exception: exec() is not allowed in this context"), f"Incorrect message prefix: {message}"
+
 
 @pytest.mark.asyncio
-async def test_exec_refusal():
-    code="""
+async def test_eval_refusal(authenticated_client: AsyncClient):
+    code = '''
 def factorial(n):
     return(n)
 eval("import os")
-"""
-    await submit_code(code, 0, "Error or Exception: eval() is not allowed in this context")
-
-
-
-
-@pytest.mark.asyncio
-async def test_incorrect_code_submit():
-    code = """
-def factorial(n):
-    return(1)
-"""
-    await submit_code(code, 0, "Test failure")
-
-
-@pytest.mark.asyncio
-async def test_correct_code_submit():
-    code = """
-def factorial(n):
-    if n < 0:
-        raise ValueError("Factorial is undefined for negative numbers.")
-    elif n == 0:
-        return 1
-    else:
-        result = 1
-        for i in range(1, n + 1):
-            result *= i
-        return result
-"""
-    await submit_code(code, 1, "Test success")
-
-# Benjamin fragen!
-@pytest.mark.asyncio
-async def test_syntax_error_handling():
-    code="""
-def factorial(n:
-    if n == 0:
-        return 1
-    else:
-        return n * factorial(n-1)
-"""
-    await submit_code(code, 0, "Error or Exception")
-
-@pytest.mark.asyncio
-async def test_import_statement_refusal():
-    code = """
-import os
-def factorial(n):
-    if n == 0:
-        return 1
-    else:
-        return n * factorial(n-1)
-"""
-    await submit_code(code, 0, "Error or Exception: Imports are not allowed in this context")
+'''
+    response = await authenticated_client.post("/api/submit", content=json.dumps({"task_unique_name": 1,
+                                                                     "code": code,
+                                                                     "log": "True",
+                                                                     "submission_id": "test",
+                                                                     "submission_time": ""}),
+                                                                     )
+    payload = response.json()
+    assert payload['test_results'][0]['status'] == 0, "Incorrect status code"
+    message = payload['test_results'][0]['message']
+    assert message.startswith("Error or Exception: eval() is not allowed in this context"), f"Incorrect message prefix: {message}"
 
 
 @pytest.mark.asyncio
-async def test_from_import_statement_refusal():
-    code="""
-from os import system
-def factorial(n):
-    if n == 0:
-        return 1
-    else:
-        return n * factorial(n-1)
-"""
-    await submit_code(code, 0, "Error or Exception: Imports are not allowed in this context")
+async def test_intersecting_skills_update(monkeypatch, authenticated_client: AsyncClient):
+    """
+    Test that the intersecting skills integration works.
+    This test mocks the judge0 call and the database calls.
+    """
+    # Mock the PFA model
+    with patch("models.domain.submissions.submissions.PFA_Model") as mock_pfa_model:
+        # Mock the execute_code_judge0 function
+        with patch("models.domain.submissions.submissions.execute_code_judge0", new_callable=AsyncMock) as mock_execute_code_judge0:
+            # Set the return value of the mock
+            mock_execute_code_judge0.return_value = '##!serialization!##{"test_result": 1, "test_message": "Test success"}##!serialization!##'
 
+            # Mock the database calls
+            with patch("db.database.get_task") as mock_get_task, \
+                 patch("db.database.get_course_enrollment") as mock_get_course_enrollment, \
+                 patch("db.database.update_course_enrollment") as mock_update_course_enrollment, \
+                 patch("db.database.log_code_submission") as mock_log_code_submission, \
+                 patch("db.database.get_course") as mock_get_course, \
+                 patch("db.database.get_courses_by_skills") as mock_get_courses_by_skills:
 
-@pytest.mark.asyncio
-async def test_delete_refusal():
-    code="""
-def factorial(n):
-    return(n)
-del test_result
-"""
-    await submit_code(code, 0, "Error or Exception: Deletes are not allowed in this context")
+                # Set the return values of the mocks
+                mock_get_task.return_value = AsyncMock(type="Function", tests={"test": "test"}, prefix="")
+                mock_get_course_enrollment.return_value = AsyncMock(tasks_completed=[], completed=False)
+                mock_get_course.return_value = AsyncMock(competencies=["Competency1"], curriculum=["greet"])
+                mock_get_courses_by_skills.return_value = AsyncMock()
 
-@pytest.mark.asyncio
-async def test_global_refusal():
-    code="""
-def factorial(n):
-    return(n)
-global test_result
-test_result = 1
-"""
-    await submit_code(code, 0, "Error or Exception: Global Scope is not allowed")
+                # Call the submit endpoint
+                response = await authenticated_client.post("/api/submit", content=json.dumps({"task_unique_name": "greet",
+                                                                             "course_unique_name": "Intro_to_Py",
+                                                                             "code": "print('hello')",
+                                                                             "log": "True",
+                                                                             "submission_id": "test",
+                                                                             "submission_time": ""}),
+                                                                             )
 
-@pytest.mark.asyncio
-async def test_nonlocal_refusal():
-    code="""
-def factorial(n):
-    test_result = 0
-    def change_result():
-        nonlocal test_result
-        test_result = 1
-    change_result()
-    return(n)
-"""
-    await submit_code(code, 0, "Error or Exception: Nonlocal Scope is not allowed")
+                # Assert that the response is successful
+                assert response.status_code == 200
 
-@pytest.mark.asyncio
-async def test_exec_refusal():
-    code="""
-def factorial(n):
-    return(n)
-exec("import os")
-"""
-    await submit_code(code, 0, "Error or Exception: exec() is not allowed in this context")
-
-@pytest.mark.asyncio
-async def test_exec_refusal():
-    code="""
-def factorial(n):
-    return(n)
-eval("import os")
-"""
-    await submit_code(code, 0, "Error or Exception: eval() is not allowed in this context")
-
+                # Assert that the PFA model was updated
+                mock_pfa_model.return_value.update_course_weights.assert_called_once()
